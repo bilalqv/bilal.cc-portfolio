d5c2a55614d60166fc9b143f47ec7d7ada8cd2d2
Bilal Ahmad Bhat <crediblebilal@gmail.com>
2024-02-03 15:43:48 +0530

    updated social urls

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,14 +1,14 @@
-export const GITHUB_URL: string = 'https://www.github.com/crediblebilal';
-export const LINKEDIN_URL: string = 'https://www.linkedin.com/in/crediblebilal';
-export const LEETCODE_URL: string = 'https://www.leetcode.com/CredibleBilal';
+export const GITHUB_URL: string = 'https://www.github.com/bilalqv';
+export const LINKEDIN_URL: string = 'https://www.linkedin.com/in/bilalqv';
+export const LEETCODE_URL: string = 'https://www.leetcode.com/bilalqv';
 export const GFG_URL: string = 'https://auth.geeksforgeeks.org/user/crediblebilal/';
 export const HACKERRANK_URL: string = 'https://www.hackerrank.com/CredibleBilal';
-export const EMAIL: string = 'mailto:crediblebilal@gmail.com';
+export const EMAIL: string = 'mailto:bilalqv@gmail.com';
 export const NIT_SRINAGAR: string = 'https://www.nitsri.ac.in/';
 export const MEDIBUDDY_URL: string = 'https://www.medibuddy.in/';
 export const SUPERBEINGS_UTL: string = 'https://www.superbeings.ai/';
-export const CHAT_APP_URL: string = 'https://github.com/crediblebilal/Realtime-Chat-App';
-export const PATIENT_PORTAL_URL: string = 'https://github.com/crediblebilal/Patient-Portal';
+export const CHAT_APP_URL: string = GITHUB_URL + '/Realtime-Chat-App';
+export const PATIENT_PORTAL_URL: string = GITHUB_URL + '/Patient-Portal';
 
 export const Achievements: string[] = [
     'Google Venkat Scholar 2022 - Selected as one of the 14 recipients of the Google Venkat Scholarship among 20,000 applicants across India.',
